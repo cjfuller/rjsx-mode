388112e8d04d5f996396c1d096bcf248275ed305
Colin Fuller <cjfuller@gmail.com>
2017-01-26 20:52:41 -0500

    Remove extra tests, edit tests to match desired style.

--- tests/bug.jsx
+++ tests/bug.jsx
@@ -1,21 +1,23 @@
 return (
-  <ModalTrigger modal={<InviteForm auth={this.props.auth} />} />
+    <ModalTrigger modal={<InviteForm auth={this.props.auth} />} />
 );
 
 return (
-  <li>
-    <ModalTrigger modal={ <InviteForm auth={this.props.auth} /> }>
-      <a href="#">Invite Representative</a>
-    </ModalTrigger>
-  </li>
+    <li>
+        <ModalTrigger modal={ <InviteForm auth={this.props.auth} /> }>
+            <a href="#">Invite Representative</a>
+        </ModalTrigger>
+    </li>
 );
 
 return (
-  <div>
-    <div>xs</div>
-    <OtherComponent/>
-    <OtherComponent class="toto"
-                    {...props}
-                    id="fr" />
-  </div>;
+    <div>
+        <div>xs</div>
+        <OtherComponent/>
+        <OtherComponent
+            class="toto"
+            {...props}
+            id="fr"
+        />
+    </div>;
 );
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 while (x) {
-  if (true) a()
-  b()
-  c()
+    if (true) a()
+    b()
+    c()
 }
 
 let newList = list.skipWhile(f)
@@ -9,12 +9,12 @@ a()
 b()
 
 Relay.createContainer(Story, {
-  initialVariables: {
-    numCommentsToShow: 10,
-    showComments: false,
-  },
-  fragments: {
-    story: (variables) => Relay.QL`
+    initialVariables: {
+        numCommentsToShow: 10,
+        showComments: false,
+    },
+    fragments: {
+        story: (variables) => Relay.QL`
       fragment on Story {
         comments(first: $numCommentsToShow) @include(if: $showComments) {
           edges {
@@ -27,79 +27,79 @@ Relay.createContainer(Story, {
         },
       }
     `,
-  }
+    }
 });
 
 var cs = {completed: this.props.todo.completed,
           editing: this.props.editing};
 
 const f = (x) =>
-  x + 1;
+      x + 1;
 
 if (a)
-  if (b)
-    if (c)
-      console.log("ici");
+    if (b)
+        if (c)
+            console.log("ici");
 console.log("la");
 
 var data = {
-  password: this.password
+    password: this.password
 }
 
 VEOrganizer.prototype.unpatchVeToolbar = function() {
-  ve.ui.Toolbar.prototype.getElementWindow =
-    this.veUiToolbarPrototypeGetElementWindow;
-  ve.ui.Toolbar.prototype.calculateOffset =
-    this.veUiToolbarPrototypeCalculateOffset;
+    ve.ui.Toolbar.prototype.getElementWindow =
+        this.veUiToolbarPrototypeGetElementWindow;
+    ve.ui.Toolbar.prototype.calculateOffset =
+        this.veUiToolbarPrototypeCalculateOffset;
 };
 
 if(!rstart.isCollapsed() ||
    !this.ve.document.hasSlugAtOffset(rstart.to)) {
-  groups = this.ve.document.getCoveredSiblingGroups(rstart);
+    groups = this.ve.document.getCoveredSiblingGroups(rstart);
 };
 
 if(!this.begin(ed.isNew() ? "savenewmodule" : "savemodule",
                ed.getSaveData(this.state.segmentId, values))) {
-  return false;
+    return false;
 }
 
 windowOffset.right = (
-  $(window).width()
-  - this.$window.outerWidth()
-  - windowOffset.left
+    $(window).width()
+        - this.$window.outerWidth()
+        - windowOffset.left
 );
 
 if(!rstart.isCollapsed()
    || this.ve.document.data.isContentOffset(rstart.to)
    || !this.ve.document.hasSlugAtOffset(rstart.to)) {
-  groups = this.ve.document.getCoveredSiblingGroups(rstart);
+    groups = this.ve.document.getCoveredSiblingGroups(rstart);
 }
 
 var json = {
-  msg: 'hello',
-  attr: condition
-      ? "This message"
-      : "Some other message"
+    msg: 'hello',
+    attr: condition
+        ? "This message"
+        : "Some other message"
 }
 
 for (let type in types)
-  doSomething(type);
+    doSomething(type);
 
 for (let type of types)
-  doSomething(type); // should be indented
+    doSomething(type); // should be indented
 
 ajax.post('/signup')
     .send(data);
 
 $(document.body)
-  .append("<span/>")
-  .attr(...);
+    .append("<span/>")
+    .attr(...);
 
 $('<div style="background: #fff">Open</div>')
-  .attr("data-id", "foo");
+    .attr("data-id", "foo");
 
 $(document.body).append("<span/>")
-                .attr(...);
+    .attr(...);
 
 var str = "toto";
 
@@ -108,86 +108,86 @@ var str = "toto";
 'test/1'.match(/[a-z]+\/[0-9]+/ug);
 
 return {
-  nowShowing: Const.ALL_TODOS,
-  editing: null
+    nowShowing: Const.ALL_TODOS,
+    editing: null
 };
 
 class Person {
-  @memoize
-  get name() { return `${this.first} ici ${this.last}` }
-  set name(val) {
-    let [first, last] = val.split(' ');
-    this.first = first;
-    this.last = last;
-  }
+    @memoize
+    get name() { return `${this.first} ici ${this.last}` }
+    set name(val) {
+        let [first, last] = val.split(' ');
+        this.first = first;
+        this.last = last;
+    }
 }
 
 
 if (true) {
-  var x;
-  /*
-   * Hello there.
-   * Today is Saturday.
-   */
-  var z;
-  /* lorem
-     ipsum */
-  var y;
+    var x;
+    /*
+     * Hello there.
+     * Today is Saturday.
+     */
+    var z;
+    /* lorem
+       ipsum */
+    var y;
 }
 
 var x = y,
     cd = 3;
 if (true) {
-  var x = y,
-      cd = 3;
-  export const {x, y} = z,
-      a = 10;
+    var x = y,
+        cd = 3;
+    export const {x, y} = z,
+    a = 10;
 }
 
 var pathData = this.barData
-                   .map(function(d) {
-                     return [{x: d.start_time_ms, vol: d.volume},
-                             {x: d.end_time_ms, vol: d.volume}];
-                   })
-                   .reduce(function(a, b) {
-                     return a.concat(b);
-                   });
+    .map(function(d) {
+        return [{x: d.start_time_ms, vol: d.volume},
+                {x: d.end_time_ms, vol: d.volume}];
+    })
+    .reduce(function(a, b) {
+        return a.concat(b);
+    });
 
 function toto(c) {
-  switch(elem) {
+    switch(elem) {
     case "textarea" :
-      return ( 1 );
-      break;
+        return ( 1 );
+        break;
     case "text" :
-      return ( 2 );
+        return ( 2 );
     default :
-      return 0;
-  }
+        return 0;
+    }
 }
 
 function hello() {
-  if (x)
-    console.log("lorem");
-  else
-    console.log("ipsum");
+    if (x)
+        console.log("lorem");
+    else
+        console.log("ipsum");
 }
 
 var obj = {
-  "a": 1,
-  "b": 2,
-  "c": 3
+    "a": 1,
+    "b": 2,
+    "c": 3
 };
 
 var arr = [1,
            2,
            3
-];
+          ];
 
 function onYouTubePlayerAPIReady(videoid) {
-  ytPlayer = new YT.Player('media_area',
-                           {videoId: videoid,
-                            playerVars: { 'rel': 0 }
-                           });
+    ytPlayer = new YT.Player('media_area',
+                             {videoId: videoid,
+                              playerVars: { 'rel': 0 }
+                             });
 }
 
 var newB = new example();
@@ -198,23 +198,23 @@ newB.firstMethod('x')
 
 var bar = 1;
 var foo = (bar == 1) ?
-          "ONE" : "OTHER";  // THIS IS THE PROBLEM
+    "ONE" : "OTHER";  // THIS IS THE PROBLEM
 
 foo() {
-  bar()
-  baz()
-  if(true)
-    hello();
-  if (true)
-    hello();
+    bar()
+    baz()
+    if(true)
+        hello();
+    if (true)
+        hello();
 }
 
 function fieldIsValid( field, fieldSpec) {
-  // todo: add remote possibility
-  var value = field.value;
-  if ( value.length < fieldSpec.minNumberOfChar ||
-       value.length > fieldSpec.maxNumberOfChar ||
-       value.match( fieldSpec.pattern) )
-    return true;
-  return false;
+    // todo: add remote possibility
+    var value = field.value;
+    if ( value.length < fieldSpec.minNumberOfChar ||
+         value.length > fieldSpec.maxNumberOfChar ||
+         value.match( fieldSpec.pattern) )
+        return true;
+    return false;
 }
