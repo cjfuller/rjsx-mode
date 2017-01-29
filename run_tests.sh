99c8353ecb01d366beb0be569ff261506ef67a1e
Colin Fuller <cjfuller@gmail.com>
2017-01-29 10:03:22 -0500

    Set up CI

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,7 @@ set -e
 emacs --batch -l ./rjsx-mode.el --eval "(progn (setq debug-on-error 1) (rjsx-mode-test))" > /dev/null
 
 set +e
+exit_status=0
 
 all_tests=`ls tests/* | grep -v "_err"`
 for test in $all_tests
@@ -14,8 +15,10 @@ do
     then
         echo ""
         echo -e "\e[31mFAILED\e[0m $test"
+        exit_status=1
         diff $test tests/_err.$testname
     else
         echo -e "\e[32mPASSED\e[0m $test"
     fi
 done
+exit $exit_status
